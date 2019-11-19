6397f500fe9de3cfe6e3e1d19d6039854a23bb98
qqLyamin <qqgithub@gmail.com>
2019-11-19 22:52:58 +0300

    как-то работает, не оптимально, без тестов

--- bignumber.cpp
+++ bignumber.cpp
@@ -3,36 +3,64 @@
 
 bigNumber::bigNumber(const QString & income)
 {
-    int outputIndex = 0;
-    int LENGTH = income.length();
-    QString tmpNumber;
-    while ( LENGTH > 0 ) {
-        for (int i = 0; i < 4 ; ++i, --LENGTH) {
-            if (LENGTH == 0) break;
-            tmpNumber += income[LENGTH - 1];
+    memset(arr, 0, SIZE_OF_ARRAY * sizeof(short));
+    bool isValid = true;
+    if (income[0] == '0' && income.length() > 1) isValid = false;
+    if (isValid) {
+        for (int i = 0; i < income.length(); ++i) {
+            if (!income[i].isNumber()) {
+                isValid = false;
+            }
         }
-        invert_string(tmpNumber);
-        arr[outputIndex] = tmpNumber.toShort();
-        outputIndex++;
-        tmpNumber = "";
     }
-    currentSize = outputIndex;
+    if (isValid) {
+        int outputIndex = 0;
+        int length = income.length();
+        QString tmpNumber;
+        while ( length > 0 ) {
+            for (int i = 0; i < 4 ; ++i, --length) {
+                if (length == 0) break;
+                tmpNumber += income[length - 1];
+            }
+            invert_string(tmpNumber);
+            arr[outputIndex] = tmpNumber.toShort();
+            outputIndex++;
+            tmpNumber = "";
+        }
+        currentSize = outputIndex;
+    } else {
+        currentSize = 1;
+    }
 }
 
 bigNumber & bigNumber::operator+(const bigNumber & other)
 {
-    bool saturation = false;
-    int SIZE;
-    SIZE = currentSize > other.currentSize ? currentSize : other.currentSize;
+    int saturation = 0;
+    int maxSize;
+    maxSize = currentSize > other.currentSize ? currentSize : other.currentSize;
 
-    for ( int i = 0 ; i < SIZE; ++i) {
-        if ( arr[i] + other.arr[i] > 9999 || (saturation == true && arr[i] + other.arr[i] > 9998) ) {
-            saturation = true;
-        } else {
-            saturation = false;
+    if (currentSize == 1 && arr[0] == 0) {
+        for (int i = 0; i < other.currentSize; ++i) {
+            this->arr[i] = other.arr[i];
         }
-        this->arr[i] = (arr[i] + other.arr[i]) % 10000;
-        if (saturation) this->arr[i] += 1;
+        currentSize = other.currentSize;
+        return * this;
+    }
+
+    if (other.currentSize == 1 && other.arr[0] == 0) {
+        return * this;
+    }
+
+    for ( int i = 0 ; i < maxSize; ++i ) {
+        short tmp = arr[i];
+        this->arr[i] = (arr[i] + other.arr[i] + saturation) % 10000;
+        saturation = (tmp + other.arr[i] + saturation) / 10000;
+    }
+
+    currentSize = maxSize;
+    if (saturation) {
+        arr[maxSize] = 1;
+        currentSize += 1;
     }
 
     return * this;
@@ -53,7 +81,17 @@ short bigNumber::getSize()
 }
 
 std::ostream& operator<< (std::ostream &out, const bigNumber & number) {
-    for (int i = number.currentSize - 1; i >= 0; --i) {
+    out << number.arr[number.currentSize - 1];
+    for (int i = number.currentSize - 2; i >= 0; --i) {
+        if (number.arr[i] < 1000) {
+            out << "0";
+        }
+        if (number.arr[i] < 100) {
+            out << "0";
+        }
+        if (number.arr[i] < 10) {
+            out << "0";
+        }
         out << number.arr[i];
     }
     return out;
--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    bigNumber B = bigNumber("9-9");
-    std::cout << B << std::endl;
-    bigNumber C = bigNumber("22222222222222222222222222222222222222222222229333333333333333333333333333333333333333333333");
+    bigNumber C = bigNumber("999999999999");
     std::cout << C << std::endl;
+    bigNumber B = bigNumber("11");
+    std::cout << B << std::endl;
     C = C + B;
     std::cout << C << std::endl;
     return a.exec();
